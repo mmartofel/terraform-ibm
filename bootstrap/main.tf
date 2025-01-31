2b40e9028786ca009157afd476e4d345ec360b7b
mmartofel <mmartofe@redhat.com>
2025-01-31 13:37:12 +0100

    version 4.9.59 updated bootstrap and http

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -6,7 +6,8 @@ terraform {
   required_providers {
     ignition = {
       source = "community-terraform-providers/ignition"
-      version = "2.1.2"
+      # version = "2.1.2"
+      version = "2.3.5"
     }
   }
 }
